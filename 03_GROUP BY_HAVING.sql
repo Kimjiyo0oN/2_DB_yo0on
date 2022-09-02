10d70a8368b9eb87d240be6614ca5acbb83daf01
Kimjiyo0oN <wldbs4166@naver.com>
2022-09-02 17:49:57 +0900

    조인 --과제

--- 03_GROUP BY_HAVING.sql
+++ 03_GROUP BY_HAVING.sql
@@ -60,11 +60,17 @@ FROM EMPLOYEE
 GROUP BY DECODE(SUBSTR(EMP_NO,8,1),'1','남','2','여') -- 별칭 사용 X(SELECT절 해석X)
 ORDER BY "인원 수"; -- 별칭 사용 O(SELECT절 해석 완료)
 
--- * WHERE 절 GROUP BY절을 
-/* 5 : SELECT
- * 1 : FROM
- *  
- * 
+-----------------------------------------------------------------------------------------------------------------------------------------
+
+-- * WHERE절 GROUP BY절을 혼합하여 사용
+--> WHERE절은 각 컬럼 값에 대한 조건 (SELECT문 해석 순서를 잘 기억하는 것이 중요!!)
+/*
+5 : SELECT
+1 : FROM
+2 : WHERE
+3 : GROUP BY
+4 : HAVING
+6 : ORDER BY
  */
 
 -- EMPLOYEE 테이블에서 부서코드가 D5, D6인 부서의 평균 급여, 인원 수 조회
@@ -80,6 +86,8 @@ FROM EMPLOYEE
 WHERE TO_CHAR(HIRE_DATE,'YYYY') >= 2000
 GROUP BY JOB_CODE;
 
+--TO_NUMBER ( TO_CHAR(HIRE_DATE,'YYYY') ) >= 2000
+--SUBSTR( TO_CHAR(HIRE_DATE, 'YYYY'), 1, 4) >= '2000'
 --WHERE EXTRACT(YEAR FROM HIRE_DATE) >= 2000
 --HIRA_DATE >= TO_DATE('2000-01-01')
 
@@ -93,7 +101,14 @@ FROM EMPLOYEE
 WHERE  HIRE_DATE >= TO_DATE('2000-01-01')
 GROUP BY JOB_CODE;--TO_CHAR(HIRE_DATE,'YYYY-MM-DD')
 
--- * 여러 컬럼을 묶어서 그룹으로 지정 가능 --> 그룹 내 그룹이 가능하다!
+-----------------------------------------------------------------------------------------------------------------------------------------
+
+-- * 여러 컬럼을 묶어서 그룹으로 지정 가능 --> 그룹 내 그룹 이 가능하다!
+-- *** GROUP BY 사용시 주의사항
+--> SELECT문에 GROUP BY절을 사용할 경우
+--  SELECT절에 명시한 조회하려는 컬럼 중
+--  그룹함수가 적용되지 않은 컬럼을 
+--  모두 GROUP BY절에 작성해야함.
 
 -- EMPLOYEE 테이블에서 부서별로 같은 직급인 사원의 수를 조회
 -- (부서코드 오름차순, 직급코드 내림차순)
@@ -105,9 +120,23 @@ GROUP BY DEPT_CODE, JOB_CODE  -- DEPT_CODE 로 그룹을 나누고,
 							  --> 세분화
 ORDER BY DEPT_CODE, JOB_CODE DESC;
 
--- * HAVING 절 
+--------------------------------------------------------------------------------------------------------------------------
+--------------------------------------------------------------------------------------------------------------------------
+
+
+-- * HAVING 절 : 그룹함수로 구해 올 그룹에 대한 조건을 설정할 때 사용
+-- HAVING 컬럼명 | 함수식 비교연산자 비교값
+/*
+5 : SELECT
+1 : FROM
+2 : WHERE
+3 : GROUP BY
+4 : HAVING
+6 : ORDER BY
+ */ 
 
 -- 부서별 평균 급여가 3백만원 이상인 부서를 조회(부서코드 오름차순)
+--sE
 SELECT DEPT_CODE , ROUND(AVG(SALARY)) 
 FROM EMPLOYEE
 GROUP BY DEPT_CODE
@@ -120,3 +149,66 @@ FROM EMPLOYEE
 GROUP BY JOB_CODE 
 HAVING COUNT(JOB_CODE) <= 5
 ORDER BY 1;
+
+-------------------------------------------------------------------------------------------------------
+-- 집계 함수(ROLL UP, CUBE)
+-- 그룹 별 산출 결과 값의 집계를 계산하는 함수 
+-- (그룹별로 중간 집계 결과를 추가)
+-- GROUP BY 절에서만 사용할 수 있는 함수!
+
+-- ROLL UP : GROUP BY 절에서 가장 먼저 작성된 컬럼의 중간 집계를 처리하는 함수
+
+SELECT DEPT_CODE, JOB_CODE, COUNT(*)
+FROM EMPLOYEE
+GROUP BY ROLLUP(DEPT_CODE, JOB_CODE)  --DEPT_CODE에 D1,  D2....에 각각 합한 값과 전체 DEPT_CODE, JOB_CODE 합한 계를 출력
+ORDER BY 1;							  -- D1 NULL 합계                                  NULL       NULL 합계 							
+
+SELECT DEPT_CODE, JOB_CODE, COUNT(*)
+FROM EMPLOYEE
+GROUP BY CUBE(DEPT_CODE, JOB_CODE); --DEPT_CODE,JOB_CODE 각각에 합한 값과 전체 DEPT_CODE, JOB_CODE  합한 계를 출력
+--ORDER BY 1;					     -- D1 NULL 합계   NULL J6 합계             NULL       NULL 합계 	
+
+
+-------------------------------------------------------------------------------------
+/* SET OPERATOR(집합 연산자)
+- 여러 SELECT의 결과(RESULT SET)를 하나의 결과로 만드는 연산자
+- UNION (합집합) : 두 SELECT 결과를 하나로 합침
+					단, 중복은 한 번만 작성
+- INTERSECT (교집합) : 두 SELECT 결과 중 중복되는 부분만 조회
+- UNION ALL : UNION + INTERSECT 
+			 합집합에서 중복 부분 제거 X
+- MINUS(차집합) : A에서 A,B 교집합 부분을 제거하고 조회
+*/
+
+-- 부서코드가 'D5'인 사원의 사번, 이름, 부서코드, 급여 
+SELECT EMP_ID, EMP_NAME, DEPT_CODE, SALARY
+FROM EMPLOYEE
+WHERE DEPT_CODE = 'D5'
+--UNION 
+--UNION ALL
+--INTERSECT 
+MINUS
+-- 급여가 300만 초과인 사원의 사번, 이름, 부서코드, 급여 
+SELECT EMP_ID, EMP_NAME, DEPT_CODE, SALARY
+FROM EMPLOYEE
+WHERE SALARY > 3000000;
+
+
+-- (주의사항!) 집합 연산자를 사용하기 위한 SELECT문들은
+-- 조회하는 컬럼의 타입, 개수가 모두 동일해야 한다!
+
+SELECT EMP_ID, EMP_NAME, DEPT_CODE, SALARY
+FROM EMPLOYEE
+WHERE DEPT_CODE = 'D5'
+UNION 
+SELECT EMP_ID, EMP_NAME, DEPT_CODE, 1
+FROM EMPLOYEE
+WHERE SALARY > 3000000;
+
+
+-- 서로 다른 테이블이지만 컬럼의 타입, 개수만 일치하면 집합 연산자 사용 가능!
+SELECT EMP_ID, EMP_NAME FROM EMPLOYEE
+UNION
+SELECT DEPT_ID, DEPT_TITLE FROM DEPARTMENT;
+
+
